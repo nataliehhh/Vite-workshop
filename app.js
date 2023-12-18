844fc859418b5d46c27064cc6d36623dbed6c1f4
natalie <hardman.natalie85@gmail.com>
2023-12-18 11:24:12 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button")
+const message = document.getElementById("message")
+
+button.addEventListener("click", function() {
+    message.textContent = ("Hello!!!")
+})
+
